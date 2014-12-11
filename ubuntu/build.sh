1bd12bf276f1f8bf4a3c6b0b089991be5308555e
Martin Schütte <martin.schuette@deck36.de>
2014-12-11 14:46:57 +0100

    update build.sh

--- ubuntu/build.sh
+++ ubuntu/build.sh
@@ -2,17 +2,17 @@
 
 TMP=`mktemp -d`
 export GOPATH=$TMP
-go get github.com/DECK36/go-log2amqp
-go build github.com/DECK36/go-log2amqp
+go get github.com/DECK36/go-log2kafka
+go build github.com/DECK36/go-log2kafka
 
-cp $TMP/bin/go-log2amqp ./log2amqp
+cp $TMP/bin/go-log2kafka ./log2kafka
 
 fpm -s dir -t deb --verbose \
-	-n deck36-log2amqp --version 0.1 --iteration 1 \
-	--url https://github.com/DECK36/go-log2amqp \
+	-n deck36-log2kafka --version 0.1 --iteration 1 \
+	--url https://github.com/DECK36/go-log2kafka \
 	--maintainer "Martin Schuette <martin.schuette@deck36.de>" \
 	--prefix /usr/local/bin \
-	--deb-default log2amqp.default \
-	--deb-upstart log2amqp.upstart \
-	log2amqp
+	--deb-default log2kafka.default \
+	--deb-upstart log2kafka.upstart \
+	log2kafka
 
